5235140e20af96b396e47cad268ff3d01856a86d
Wherkamp <wherkamp@gmail.com>
2017-12-07 11:30:30 -0500

    Initial commit

---
+++ src/main/java/me/kingtux/mcbitcoin/McBitCoin.java
@@ -0,0 +1,17 @@
+package me.kingtux.mcbitcoin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McBitCoin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
