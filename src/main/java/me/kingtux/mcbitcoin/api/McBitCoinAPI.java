d5a9b4c27f8bf07ea97a2794d3b1a0ffe91c1187
Wherkamp <wherkamp@gmail.com>
2017-12-07 20:45:21 -0500

    API part of managing money started.

--- src/main/java/me/kingtux/mcbitcoin/api/McBitCoinAPI.java
+++ src/main/java/me/kingtux/mcbitcoin/api/McBitCoinAPI.java
@@ -1,15 +1,45 @@
 package me.kingtux.mcbitcoin.api;
 
 import me.kingtux.mcbitcoin.McBitCoin;
+import org.bukkit.entity.Player;
 
 public class McBitCoinAPI {
-    private McBitCoin McBitCoin;
+    private McBitCoin mcBitCoin;
 
     public McBitCoinAPI(McBitCoin plugin){
-        this.McBitCoin = plugin;
+        this.mcBitCoin = plugin;
     }
 
     public McBitCoin getMcBitCoin() {
-        return McBitCoin;
+        return mcBitCoin;
+    }
+
+    public int getBalance(Player p) {
+        if (mcBitCoin.getConfigSettings().isUseMySql()) {
+            return mcBitCoin.getConnectionManager().getPlayerAccountBalance(p);
+        } else {
+            return 0;
+        }
+    }
+
+    public void setBalance(Player p, int amount) {
+        if (mcBitCoin.getConfigSettings().isUseMySql()) {
+            mcBitCoin.getConnectionManager().setMoney(p, amount);
+        } else {
+        }
+    }
+
+    public void addBalance(Player p, int amount) {
+        if (mcBitCoin.getConfigSettings().isUseMySql()) {
+            mcBitCoin.getConnectionManager().addMoney(p, amount);
+        } else {
+        }
+    }
+
+    public void subtractBalance(Player p, int amount) {
+        if (mcBitCoin.getConfigSettings().isUseMySql()) {
+            mcBitCoin.getConnectionManager().removeMoney(p, amount);
+        } else {
+        }
     }
 }
