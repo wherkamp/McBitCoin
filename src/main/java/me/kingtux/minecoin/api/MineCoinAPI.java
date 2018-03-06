70db457bd1621b097bc21a2c58b7fc62db555a58
wherkamp <wherkamp@gmail.com>
2018-03-06 14:56:58 -0500

    1.1 Update

--- src/main/java/me/kingtux/minecoin/api/MineCoinAPI.java
+++ src/main/java/me/kingtux/minecoin/api/MineCoinAPI.java
@@ -17,7 +17,6 @@ public class MineCoinAPI {
     }
 
     /**
-     *
      * @return The instance of MineCoinAPI
      */
     public static MineCoinAPI getInstance() {
@@ -25,7 +24,6 @@ public class MineCoinAPI {
     }
 
     /**
-     *
      * @return The MineCoinMain instance
      */
     public MinecoinMain getMinecoinMain() {
@@ -33,11 +31,13 @@ public class MineCoinAPI {
     }
 
     /**
-     *
-     * @param p
+     * @param p The player to get the balance of
      * @return The balance of the player
      */
     public int getBalance(Player p) {
+        if (p == null) {
+            return 0;
+        }
         if (minecoinMain.getConfigSettings().useMySql()) {
             return minecoinMain.getConnectionManager().getPlayerAccountBalance(p);
         } else {
@@ -46,12 +46,14 @@ public class MineCoinAPI {
     }
 
     /**
-     *
-     * @param p The player
+     * @param p      The player
      * @param amount The new Balance
      * @return If it was a success
      */
     public boolean setBalance(Player p, int amount) {
+        if (p == null) {
+            return false;
+        }
         if (minecoinMain.getConfigSettings().useMySql()) {
             minecoinMain.getConnectionManager().setMoney(p, amount);
             return true;
@@ -64,12 +66,14 @@ public class MineCoinAPI {
     }
 
     /**
-     *
-     * @param p The player
+     * @param p      The player
      * @param amount The amount
      * @return True or false if was success
      */
     public boolean addBalance(Player p, int amount) {
+        if (p == null) {
+            return false;
+        }
 
         if (minecoinMain.getConfigSettings().useMySql()) {
             minecoinMain.getConnectionManager().addMoney(p, amount);
@@ -83,12 +87,14 @@ public class MineCoinAPI {
     }
 
     /**
-     *
-     * @param p The player
+     * @param p      The player
      * @param amount amount to remmove
      * @return true or false if transaction was a success
      */
     public boolean subtractBalance(Player p, int amount) {
+        if (p == null) {
+            return false;
+        }
         if (amount > getBalance(p)) {
             if (minecoinMain.getConfigSettings().useMySql()) {
                 minecoinMain.getConnectionManager().removeMoney(p, amount);
@@ -104,11 +110,15 @@ public class MineCoinAPI {
     }
 
     /**
-     * @apiNote Doing this could ruin the data
+     * apiNote Doing this could ruin the data
+     *
      * @param player The player to create an account for
      * @return if was a success
      */
     public boolean createAccount(Player player) {
+        if (player == null) {
+            return false;
+        }
         if (minecoinMain.getConfigSettings().useMySql()) {
             minecoinMain.getConnectionManager().createPlayerAccount(player);
             return true;
