79e4110fb2581225b3a8820049e99349d1d2dd0b
wherkamp <wherkamp@kingtux.me>
2018-10-28 11:10:45 -0400

    CoinECO updated to new LangFile

--- src/main/java/me/kingtux/minecoin/commands/CoinecoCommand.java
+++ src/main/java/me/kingtux/minecoin/commands/CoinecoCommand.java
@@ -2,8 +2,10 @@ package me.kingtux.minecoin.commands;
 
 import me.kingtux.minecoin.MineCoinMain;
 import me.kingtux.minecoin.Utils;
+import me.kingtux.minecoin.lang.LangFile;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,70 +15,89 @@ import java.util.concurrent.ExecutionException;
 
 public class CoinecoCommand implements CommandExecutor {
 
-  private MineCoinMain mineCoinMain;
+    private MineCoinMain mineCoinMain;
 
-  public CoinecoCommand(MineCoinMain pl) {
-    mineCoinMain = pl;
-  }
+    public CoinecoCommand(MineCoinMain pl) {
+        mineCoinMain = pl;
+    }
 
-  @Override
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (args.length < 3) {
-      sender.sendMessage(
-          getString("messages.not-enough-args").replace("{format}", command.getUsage()));
-      return true;
-    }
-    if (!sender.hasPermission("minecoin.coineco.use")) {
-      sender.sendMessage(Utils.color(getString("messages.you-lack-permissions")));
-      return true;
-    }
-    Player reciever = Bukkit.getPlayer(args[1]);
-    if (reciever == null) {
-      sender.sendMessage(
-          Utils.color(getString("messages.player-not-online").replace("name", args[0])));
-      return true;
-    }
-    if (args[0].equalsIgnoreCase("set")) {
-      mineCoinMain.getAPIManager().setBalance(reciever, Integer.parseInt(args[2]));
-      sendMessage(sender, reciever, Integer.parseInt(args[2]), "set");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 3) {
+            sender.sendMessage(LangFile.COIN_ECO_FORMAT.getColorValue());
+            return true;
+        }
+        if (!sender.hasPermission("minecoin.coineco.use")) {
+            sender.sendMessage(LangFile.LACK_PERMS.getColorValue());
+            return true;
+        }
+        Player reciever = Bukkit.getPlayer(args[1]);
+        if (reciever == null) {
+            sender.sendMessage(
+                    Utils.color(LangFile.MISSING_PLAYER.getColorValue().replace("name", args[0])));
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("set")) {
+            mineCoinMain.getAPIManager().setBalance(reciever, Integer.parseInt(args[2]));
+            sendMessage(sender, reciever, Integer.parseInt(args[2]), "set");
 
-    } else if (args[0].equalsIgnoreCase("add")) {
-      mineCoinMain.getAPIManager().addBalance(reciever, Integer.parseInt(args[2]));
-      sendMessage(sender, reciever, Integer.parseInt(args[2]), "add");
-    } else if (args[0].equalsIgnoreCase("subtract")) {
-      boolean result = false;
-      try {
-        result = mineCoinMain.getAPIManager()
-            .subtractBalance(reciever, Integer.parseInt(args[2])).get();
-      } catch (InterruptedException | ExecutionException e) {
-        e.printStackTrace();
-      }
-      if (result) {
-        sendMessage(sender, reciever, Integer.parseInt(args[2]), "subtract");
-      } else {
+        } else if (args[0].equalsIgnoreCase("add")) {
+            mineCoinMain.getAPIManager().addBalance(reciever, Integer.parseInt(args[2]));
+            sendMessage(sender, reciever, Integer.parseInt(args[2]), "add");
+        } else if (args[0].equalsIgnoreCase("subtract")) {
+            boolean result = false;
+            try {
+                result = mineCoinMain.getAPIManager()
+                        .subtractBalance(reciever, Integer.parseInt(args[2])).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            if (result) {
+                sendMessage(sender, reciever, Integer.parseInt(args[2]), "subtract");
+            } else {
 //low-on-funds
-        sender.sendMessage(
-            getString("messages.low-on-funds.other")
-                .replace("{player}", reciever.getDisplayName()));
-      }
-    } else {
-      //invalid-format
-      sender.sendMessage(
-          getString("messages.invalid-format").replace("{format}", command.getUsage()));
+                sender.sendMessage(
+                        LangFile.LACK_OF_FUNDING_OTHER.getColorValue()
+                                .replace("{player}", reciever.getDisplayName()));
+            }
+        } else {
+            //invalid-format
+            sender.sendMessage(LangFile.COIN_ECO_FORMAT.getColorValue());
+        }
+        return true;
     }
-    return true;
-  }
 
-  private void sendMessage(CommandSender sender, Player reciever, int amount, String job) {
-    sender.sendMessage(Utils
-        .color(Utils
-            .format(sender, reciever, amount, getString("messages.coineco." + job + ".sender"))));
-    reciever.sendMessage(Utils
-        .color(Utils
-            .format(sender, reciever, amount, getString("messages.coineco." + job + ".receiver"))));
-  }
+    private void sendMessage(CommandSender sender, Player receiver, int amount, String job) {
+        sender.sendMessage(Utils.placeholderAPI(Utils
+                .color(Utils
+                        .format(sender, receiver, amount, getSFormat(job))), (OfflinePlayer) sender));
+        receiver.sendMessage(Utils.placeholderAPI(Utils
+                .color(Utils
+                        .format(sender, receiver, amount, getRFormat(job))), (OfflinePlayer) sender));
+    }
 
-  private String getString(String key) {
-    return mineCoinMain.getConfig().getString(key);
-  }
+    private String getRFormat(String job) {
+        switch (job) {
+            case "add":
+                return LangFile.COIN_ECO_ADD_RECEIVER.getColorValue();
+            case "subtract":
+                return LangFile.COIN_ECO_SUBTRACT_RECEIVER.getColorValue();
+            case "set":
+                return LangFile.COIN_ECO_SET_RECEIVER.getColorValue();
+        }
+        return "";
+    }
+
+    private String getSFormat(String job) {
+        switch (job) {
+            case "add":
+                return LangFile.COIN_ECO_ADD_SENDER.getColorValue();
+            case "subtract":
+                return LangFile.COIN_ECO_SUBTRACT_SENDER.getColorValue();
+            case "set":
+                return LangFile.COIN_ECO_SET_SENDER.getColorValue();
+        }
+        return "";
+    }
+
 }
--- src/main/java/me/kingtux/minecoin/lang/LangFile.java
+++ src/main/java/me/kingtux/minecoin/lang/LangFile.java
@@ -15,7 +15,27 @@ public enum LangFile {
     @ConfigEntry(path = "messages.balance.personal")
     BALANCE_MESSAGE_YOU("Your balance is {balance}"),
     @ConfigEntry(path = "messages.balance.other")
-    BALANCE_MESSAGE_OTHER("{player} balance is {balance}");
+    BALANCE_MESSAGE_OTHER("{player} balance is {balance}"),
+    @ConfigEntry(path = "messages.coin-eco.add.sender")
+    COIN_ECO_ADD_SENDER(""),
+    @ConfigEntry(path = "messages.coin-eco.set.sender")
+    COIN_ECO_SET_SENDER(""),
+    @ConfigEntry(path = "messages.coin-eco.subtract.sender")
+    COIN_ECO_SUBTRACT_SENDER(""),
+    @ConfigEntry(path = "messages.coin-eco.add.receiver")
+    COIN_ECO_ADD_RECEIVER(""),
+    @ConfigEntry(path = "messages.coin-eco.set.receiver")
+    COIN_ECO_SET_RECEIVER(""),
+    @ConfigEntry(path = "messages.coin-eco.subtract.receiver")
+    COIN_ECO_SUBTRACT_RECEIVER(""),
+    @ConfigEntry(path="messages.funding.lack.you")
+    LACK_OF_FUNDING("You lack the money to do that!"),
+    @ConfigEntry(path="messages.coin-eco.format")
+    COIN_ECO_FORMAT("Invalid Format please use /coineco <add,set,subtract> {who} {amount}"),
+    @ConfigEntry(path="messages.funding.lack.other")
+    LACK_OF_FUNDING_OTHER("{player} lack the money to do that!");
+
+
     @ConfigValue
     private String value;
 
